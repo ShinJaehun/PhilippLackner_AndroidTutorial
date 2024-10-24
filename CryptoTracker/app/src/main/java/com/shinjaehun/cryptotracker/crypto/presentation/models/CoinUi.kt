d9d3334bee94786752f3fdfbc83099da47e5cd29
ShinJaehun <gunsmoke.shin@gmail.com>
2024-10-24 22:37:11 +0900

    CryptoTracker: coin list screen

---
+++ CryptoTracker/app/src/main/java/com/shinjaehun/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -0,0 +1,47 @@
+package com.shinjaehun.cryptotracker.crypto.presentation.models
+
+import androidx.annotation.DrawableRes
+import com.shinjaehun.cryptotracker.core.presentation.util.getDrawableIdForCoin
+import com.shinjaehun.cryptotracker.crypto.domain.Coin
+import java.text.NumberFormat
+import java.util.Locale
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCapUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val changePercent24Hr: DisplayableNumber,
+    @DrawableRes val iconRes: Int
+)
+
+data class DisplayableNumber(
+    val value: Double,
+    val formatted: String
+)
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
