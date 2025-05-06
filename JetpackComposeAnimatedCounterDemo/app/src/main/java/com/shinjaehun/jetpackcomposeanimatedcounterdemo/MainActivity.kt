2d81b7b14924c724887a128ba5e5e850ba6c9f7e
ShinJaehun <gunsmoke.shin@gmail.com>
2025-05-06 11:30:35 +0900

    finish jetpack compose animated counter demo

---
+++ JetpackComposeAnimatedCounterDemo/app/src/main/java/com/shinjaehun/jetpackcomposeanimatedcounterdemo/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.shinjaehun.jetpackcomposeanimatedcounterdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shinjaehun.jetpackcomposeanimatedcounterdemo.ui.theme.JetpackComposeAnimatedCounterDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeAnimatedCounterDemoTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    var count by remember {
+                        mutableStateOf(0)
+                    }
+                    AnimatedCounter(
+                        count = count,
+                        style = MaterialTheme.typography.headlineLarge
+                    )
+                    Button(onClick = { count++ }) {
+                        Text(text = "Increment")
+                    }
+                }
+            }
+        }
+    }
+}
